9782ecf8cceb922e27ae9a82a4396987ebb37e40
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-05-26 14:18:13 +0530

    ms01-common: cleanup BOARD_KERNEL_CMDLINE
    - Just to avoid line wrapping
    - Add commented out permissive boot arg for convenience

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,9 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4 maxcpus=1
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x37 
+BOARD_KERNEL_CMDLINE += zcache.enabled=1 zcache.compressor=lz4 maxcpus=1
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
